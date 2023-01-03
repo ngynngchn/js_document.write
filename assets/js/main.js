f7ea2935d450d7ed2605e6d65501485555c10cf2
Hanni <ngynngchn@gmail.com>
2023-01-03 14:31:56 +0100

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+document.write("<h1>Hello World!</h1>");
+document.write("<p>Have a nice day!</p>");
+let myText = "Hello again";
+
+let vorname = "Ngoc Han";
+let nachname = "Nguyen";
+document.write(`<p>${myText}</p>`);
+document.write(`<p> Name: ${vorname}</p>`);
+document.write(`<p> Nachname: ${nachname}</p>`);
+
+// The write() method deletes all existing HTML when used on a loaded document.
